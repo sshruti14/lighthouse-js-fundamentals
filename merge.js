ebd0091791f3647a46e7a8ffdf439397b8c7e32a
Shruti Shetty <shetty_14august@yahoo.com>
2020-08-18 12:15:43 -0400

    test

---
+++ merge.js
@@ -0,0 +1,11 @@
+function merge(arr1,arr2) {
+
+  for(var i=0;i<arr2.lenght;i++){
+arr1.push();
+  }
+
+}
+
+console.log(merge([4, 5, 6], [1, 2, 3, 4]), "=?", [1, 2, 3, 4, 4, 5, 6]);
+console.log(merge([4], [2, 5, 8]), "=?", [2, 4, 5, 8]);
+console.log(merge([1, 2, 6], []), "=?", [1, 2, 6]);
